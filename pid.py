e5e3a6e951ad134ae06d48ceadf5e785aed970ed
Neeme Takis <neeme@takis.ee>
2014-01-08 09:51:29 +0200

    a few improvements

--- pid.py
+++ pid.py
@@ -22,13 +22,13 @@ class PID:
     """ Simple PID control.
         This class implements a simplistic PID control algorithm.
     """
-    def __init__(self, SP=0, P=2.0, I=1.0, D=0.0, min=-9999, max=9999): # initialize gains
-        self.Kp=P
-        self.Ki=I
-        self.Kd=D
-        self.setPoint=SP
-        self.Min=min
-        self.Max=max
+    def __init__(self, SP=0, P=2.0, I=1.0, D=0.0, min=None, max=None): # initialize gains
+        self.setSetpoint(SP)
+        self.setKp(P)
+        self.setKi(I)
+        self.setKd(D)
+        self.setMin(min)
+        self.setMax(max)
         self.Initialize()
 
     def setKp(self, invar): # Set proportional gain
@@ -36,7 +36,7 @@ class PID:
 
     def setKi(self, invar): # Set integral gain
         self.Ki = invar
-        
+
     def resetIntegral(self): # reset integral part
         self.Ci = 0
 
@@ -45,13 +45,13 @@ class PID:
 
     def setPrevErr(self, preverr): # Set previous error value
         self.prev_err = preverr
-        
+
     def setMin(self, outMin): # Set lower limit for output
-        self.Min = outMin
+        self.outMin = outMin
 
     def setMax(self, outMax): # Set lower limit for output
-        self.Max = outMax
-        
+        self.outMax = outMax
+
     def setSetpoint(self, setPoint): # Set lower limit for output
         self.setPoint = setPoint
 
@@ -59,9 +59,7 @@ class PID:
         self.currtm = time.time()
         self.prevtm = self.currtm
         self.prev_err = 0
-        #self.setpoint = 0
         self.onLimit=0 # value 0 means between limits, 1 on lo limit, 2 on hi limit
-        
         # term result variables
         self.Cp = 0
         self.Ci = 0
@@ -81,7 +79,7 @@ class PID:
         if self.onLimit == 0 or (self.onLimit ==1 and error>0) or (self.onLimit == 2 and error<0): # no limit reached or moving away from limit
             self.onLimit=0
             self.Ci += error * dt                   # integral term
-        
+
         self.Cd = 0
         if dt > 0:                              # no div by zero
             self.Cd = de/dt                     # derivative term
@@ -91,12 +89,13 @@ class PID:
 
         # sum the terms and return the result
         Output=self.Cp + (self.Ki * self.Ci) + (self.Kd * self.Cd)
-        if Output > self.Max:
-            self.onLimit=2 # reached hi limit
-            Output=self.Max
-        if Output < self.Min:
-            self.onLimit=1 # reached lo limit
-            Output=self.Min
-           
+        if self.outMax != None:
+            if Output > self.outMax:
+                self.onLimit=2 # reached hi limit
+                Output=self.outMax
+        if self.outMin != None:
+            if Output < self.outMin:
+                self.onLimit=1 # reached lo limit
+                Output=self.outMin
+
         return Output,self.Cp,(self.Ki * self.Ci),(self.Kd * self.Cd),error,self.onLimit # [out,p,i,d,e,limit]
-         
